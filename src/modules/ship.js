aba51dfeac8f7098312a4bccaa1d561e290b6c28
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-20 04:53:36 +0530

    feat: define isSunk() function
    isSunk() calculates whether a ship is considered sunk based on its length and the number of hits it has received.

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -2,5 +2,12 @@ export default function createShip(length, hitCount, sinkStatus) {
  const hit = () => {
     hitCount++;
  };
-    
+ 
+ const isSunk = () => {
+    if (hitCount === length) {
+        return true;
+    } else {
+        return false;
+    }
+ };
 }
