dc8eab1cab55afc5e05cdd8035545d6b99e6ff80
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-20 20:41:55 +0530

    test(gameBoard): test if ships are placed rightly

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -5,10 +5,23 @@ describe('Game board', () => {
     let gameBoard;
 
     beforeEach(() => {
-      gameBoard = createGameboard();
+      gameBoard = createGameBoard();
     });
 
+    test('places ships accurately', () => {
+        gameBoard.placeShip(2, [[1, 0], [1, 1]]);
 
+        const fleet = gameBoard.getFleet();
 
-
+        expect(fleet).toEqual([
+            {
+                ship: {
+                    length: 2,
+                    hit: expect.any(Function),
+                    isSunk: expect.any(Function)
+                },
+                coordinatesOfShip: [[1, 0], [1, 1]],
+            }
+        ]);
+    });
 });
