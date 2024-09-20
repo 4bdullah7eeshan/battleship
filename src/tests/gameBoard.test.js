96ec91999c7fd87e801b9d293143274f1c105d2a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-20 20:27:40 +0530

    feat: initialize beforeEach() block

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,3 +1,14 @@
 import createShip from "../modules/ship.js";
 import createGameBoard from "../modules/gameBoard.js";
 
+describe('Game board', () => {
+    let gameboard;
+
+    beforeEach(() => {
+      gameboard = createGameboard();
+    });
+
+    
+
+
+});
