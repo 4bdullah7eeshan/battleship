84b87797f1679284e5435af942aba2b879c54d7c
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-21 01:00:10 +0530

    feat: setup 'real' player
    For a real player, essentially we want to give freedom to attack anywhere.

--- src/modules/player.js
+++ src/modules/player.js
@@ -3,4 +3,19 @@ import createGameBoard from "./gameBoard.js";
 export default function createPlayer(playerType) {
    const gameBoard = createGameBoard();
 
+   if (playerType === "real") {
+    // for real player
+    const realPlayerAttack = (opponentGameBoard, x, y) => {
+        return opponentGameBoard.receiveAttack(x, y);
+    };
+
+    return {
+        gameBoard,
+        realPlayerAttack,
+    };
+
+   } else {
+    // for computer
+   }
+
 }
