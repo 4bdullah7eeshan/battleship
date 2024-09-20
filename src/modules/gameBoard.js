36a221cedead019550a21bac9e32cb891dac6444
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-20 16:00:37 +0530

    feat: add parameters to placeShip function
    In order to place a ship on the game board, we need two things: ship's length and its coordinates.

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -2,7 +2,7 @@ import createShip from "./ship.js";
 
 export default function createGameBoard() {
     
-    const placeShip = () => {
+    const placeShip = (lengthOfShip, coordinatesOfShip) => {
         // This will place the ships on the game board
     };
 }
