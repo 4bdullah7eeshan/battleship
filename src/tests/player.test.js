ddd0dd7aecefc45c7e90b7dae16b83d5b11adf11
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-21 01:46:05 +0530

    test(player): setup different types of players

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -2,5 +2,12 @@ import createPlayer from "../modules/player.js";
 import createGameBoard from "../modules/gameBoard.js";
 
 describe('Player factory', () => {
+    let realPlayer, computerPlayer, opponent;
+
+    beforeEach(() => {
+        realPlayer = createPlayer("real");
+        computerPlayer = createPlayer("computer");
+        opponent = createPlayer("real");
+    });
 
 });
