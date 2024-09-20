06204905e42212c8cbcace09f4088fbf8d5fe10e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-21 01:27:11 +0530

    feat: setup 'computer' player

--- src/modules/player.js
+++ src/modules/player.js
@@ -3,24 +3,29 @@ import createGameBoard from "./gameBoard.js";
 export default function createPlayer(playerType) {
    const gameBoard = createGameBoard();
 
-   // An 'attack' is common for both 'real' player and a 'computer'
    const attack = (opponentGameBoard, x, y) => {
         return opponentGameBoard.receiveAttack(x, y);
    };
 
    if (playerType === "real") {
-    // for real player
     return {
         gameBoard,
         attack,
     };
 
    } else {
-    // for computer
     const pickRandomCoordinate = () => {
         return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
     };
-    
-   }
 
+    const computerAttack = (opponentGameBoard) => {
+        const [x, y] = pickRandomCoordinate();
+        return attack(opponentGameBoard, x, y);
+    };
+
+    return {
+        gameBoard,
+        computerAttack,
+    };
+   }
 }
