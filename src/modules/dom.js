286354ae0b55653407dca3ee147507d5ae18c518
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-21 03:39:31 +0530

    Add notes to self

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,3 +1,6 @@
 const dom = (function () {
-
+    // This should do the following things:
+    // 1. Render app
+    // 2. Add event listeners
+    // Maintain another module for controlling the game. This should do pure DOM stuff
 })();
