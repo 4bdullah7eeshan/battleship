ba3644759e30d243ff91095e698c0548e23b4656
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-20 15:52:48 +0530

    feat: declare placeShip() function
    This will be responsible for placing ships on the game board

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,3 +1,8 @@
+import createShip from "./ship.js";
+
 export default function createGameBoard() {
     
+    const placeShip = () => {
+        // This will place the ships on the game board
+    };
 }
