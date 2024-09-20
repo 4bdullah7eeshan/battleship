3221bbe6bfce150dbb90f22de0345d8746656ecc
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-20 15:00:50 +0530

    test(ship): test ship initialization
    When a ship object is created with createShip(l), its length should be 'l' and isSunk() should be false.

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,1 +1,7 @@
 import createShip from "../modules/ship.js";
+
+test("Initialize ship with correct length and hitCount", () => {
+    const ship = createShip(5);
+    expect(ship.length).toBe(5);
+    expect(ship.isSunk()).toBe(false);
+});
