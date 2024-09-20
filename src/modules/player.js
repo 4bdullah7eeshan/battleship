2846f2871961cf27471d1fb848e035aa32c3780e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-21 01:24:33 +0530

    refactor: make the attack method common
    An 'attack' is common for both 'real' player and a 'computer'.

--- src/modules/player.js
+++ src/modules/player.js
@@ -3,15 +3,16 @@ import createGameBoard from "./gameBoard.js";
 export default function createPlayer(playerType) {
    const gameBoard = createGameBoard();
 
+   // An 'attack' is common for both 'real' player and a 'computer'
+   const attack = (opponentGameBoard, x, y) => {
+        return opponentGameBoard.receiveAttack(x, y);
+   };
+
    if (playerType === "real") {
     // for real player
-    const realPlayerAttack = (opponentGameBoard, x, y) => {
-        return opponentGameBoard.receiveAttack(x, y);
-    };
-
     return {
         gameBoard,
-        realPlayerAttack,
+        attack,
     };
 
    } else {
@@ -19,6 +20,7 @@ export default function createPlayer(playerType) {
     const pickRandomCoordinate = () => {
         return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
     };
+    
    }
 
 }
