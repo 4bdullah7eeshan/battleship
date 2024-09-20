3d6a07998769eb4f3060f0b09d15e37c831544de
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-20 15:31:43 +0530

    refactor: nest the tests in describe blocks

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,20 +1,26 @@
 import createShip from "../modules/ship.js";
 
-test("Initialize ship with correct length and hitCount", () => {
-    const ship = createShip(5);
-    expect(ship.length).toBe(5);
-    expect(ship.isSunk()).toBe(false);
-});
+describe("Ship factory", () => {
+    describe('Initialization', () => {
+        test("Initialize ship with correct length and hitCount", () => {
+            const ship = createShip(5);
+            expect(ship.length).toBe(5);
+            expect(ship.isSunk()).toBe(false);
+        });
+    });
 
-test('Check if ship is not sunk until all hits received', () => {
-    const ship = createShip(3);
+    describe('Ship sunk status', () => {
+        test('Check if ship is not sunk until all hits received', () => {
+            const ship = createShip(3);
 
-    ship.hit();    
-    expect(ship.isSunk()).toBe(false);
+            ship.hit();    
+            expect(ship.isSunk()).toBe(false);
 
-    ship.hit();
-    expect(ship.isSunk()).toBe(false);
-    
-    ship.hit();
-    expect(ship.isSunk()).toBe(true);
+            ship.hit();
+            expect(ship.isSunk()).toBe(false);
+            
+            ship.hit();
+            expect(ship.isSunk()).toBe(true);
+        });
+    });
 });
