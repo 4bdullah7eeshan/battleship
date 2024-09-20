678723bd979f4d1579bef663be47ff7e44eebace
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-20 16:13:27 +0530

    feat: declare receiveAttack() function
    How should the coordinated be passed here?

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -8,4 +8,8 @@ export default function createGameBoard() {
         const ship = createShip(lengthOfShip);
         fleet.push({ ship, coordinatesOfShip });
     };
+
+    const receiveAttack = () => {
+
+    };
 }
