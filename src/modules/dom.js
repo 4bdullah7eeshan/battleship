70aa9ac1e63d793f3427bf5faa5b03880adbca59
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-21 15:51:09 +0530

    feat(ui): define function to generate a 10x10 grid

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -8,5 +8,16 @@ const dom = (function () {
     const modal = document.querySelector('dialog');
     const startButton = document.getElementById('start-game');
 
-    
+    const generateGrid = (boardElement) => {
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                // O(n^2) is fine for now! Will refactor this later.
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.dataset.x = i;
+                cell.dataset.y = j;
+                boardElement.appendChild(cell);
+            }
+        }
+    };
 })();
