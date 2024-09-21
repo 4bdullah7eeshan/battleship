b30e6c778ce5d82b62e46042b606b69b36783175
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-21 16:00:54 +0530

    feat(ui): create a function to drive game setup

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -48,3 +48,5 @@ const dom = (function () {
         onStartGame,
     };
 })();
+
+export default dom;
--- src/modules/gameDriver.js
+++ src/modules/gameDriver.js
@@ -1,1 +1,20 @@
 // to drive the game
+import dom from "./dom.js";
+
+const gameDriver = (() => {
+    let player, computer;
+    const initializeGame = () => {
+        player = createPlayer('real');
+        computer = createPlayer('computer');
+
+        dom.onRandomPlaceShips(() => {
+            player.gameBoard.randomlyPlaceShips();
+            dom.closeModal();
+        });
+
+        dom.onStartGame(() => {
+            startGame();
+        });
+    };
+
+})();
