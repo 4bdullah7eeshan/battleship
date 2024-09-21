a4b2020b530ab433d029c7451b12c2614f7c3dc7
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-21 16:05:29 +0530

    featL setup entry point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // index.js
+import './css/reset.css';
+import domModule from './modules/dom.js';
+import gameDriver from './modules/gameDriver.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+    domModule.setupUI();
+    gameDriver.initializeGame();
+});
